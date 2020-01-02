99bac5f5ebabea2a0e9d3990027a3a3dedf2b246
Christopher Lamm <chris@theantichris.com>
2020-01-02 09:17:31 -0500

    defined method with base case

---
+++ tree.py
@@ -0,0 +1,7 @@
+def build_bst(my_list):
+    if len(my_list) == 0:
+            return "No Child"
+
+sorted_list = [12, 13, 14, 15, 16]
+binary_search_tree = build_bst(sorted_list)
+print(binary_search_tree)
